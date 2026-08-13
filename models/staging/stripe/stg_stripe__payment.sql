45de04d410f71e3d95667f03baa5e1f1a55d37c3
SALOMEZ Clémence <csalomez@actinvision.com>
2026-08-13 09:01:17 +0000

    tests

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -2,19 +2,20 @@ with
 
 source as (
 
-    select * from raw.stripe.payment
+    select * from {{ source('stripe', 'payment') }}
 
 ),
 
 renamed as (
 
     select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status as payment_status,
-        amount as payment_amount,
-        created as payment_created,
+        id                  as payment_id,
+        orderid             as order_id,
+        paymentmethod       as payment_method,
+        status              as payment_status,
+    -- amount is stored in cents, convert it to dollars
+        amount / 100        as payment_amount,
+        created             as payment_created,
         _batched_at
 
     from source
